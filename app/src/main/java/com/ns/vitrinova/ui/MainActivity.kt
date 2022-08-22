bb8f8c92284ac0de90fdb9209ba407cec351f658
enesarisoy <arisoyeness@gmail.com>
2022-08-22 12:15:28 +0300

    splash screen

--- app/src/main/java/com/ns/vitrinova/ui/MainActivity.kt
+++ app/src/main/java/com/ns/vitrinova/ui/MainActivity.kt
@@ -3,8 +3,6 @@ package com.ns.vitrinova.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.core.splashscreen.SplashScreen
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ns.vitrinova.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,21 +10,14 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        initSplash()
+
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
-    private fun initSplash() {
-        installSplashScreen().apply {
-            setKeepOnScreenCondition {
-                viewModel.isLoading.value
-            }
-        }
-    }
+
 }
---
+++ app/src/main/java/com/ns/vitrinova/ui/splash/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.ns.vitrinova.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ns.vitrinova.R
+import com.ns.vitrinova.ui.MainActivity
+import kotlinx.coroutines.*
+
+class SplashActivity : AppCompatActivity() {
+
+    val activityScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        activityScope.launch {
+            delay(3000)
+
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onPause() {
+        activityScope.cancel()
+        super.onPause()
+    }
+}
